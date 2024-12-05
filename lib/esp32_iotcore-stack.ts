8920cb3aa36a0401f974220c0045f686be886b5a
raiha <68338762+raihalea@users.noreply.github.com>
2024-12-06 03:21:52 +0900

    update

--- lib/esp32_iotcore-stack.ts
+++ lib/esp32_iotcore-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { IotSql, TopicRule } from "@aws-cdk/aws-iot-alpha";
+import { CloudWatchPutMetricAction } from "@aws-cdk/aws-iot-actions-alpha";
 
 export class Esp32IotcoreStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'Esp32IotcoreQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const topicRule = new TopicRule(this, "TopicRule", {
+      sql: IotSql.fromStringAsVerNewestUnstable(
+        "SELECT co2, temperature, humidity FROM 'esp32/sensorData'"
+      ),
+      actions: [
+        new CloudWatchPutMetricAction({
+          metricName: "co2",
+          metricNamespace: "IoT/Esp32/SensorData",
+          metricUnit: "Count",
+          metricValue: "${co2}",
+        }),
+        new CloudWatchPutMetricAction({
+          metricName: "temperature",
+          metricNamespace: "IoT/Esp32/SensorData",
+          metricUnit: "Count",
+          metricValue: "${temperature}",
+        }),
+        new CloudWatchPutMetricAction({
+          metricName: "humidity",
+          metricNamespace: "IoT/Esp32/SensorData",
+          metricUnit: "humidity",
+          metricValue: "${humidity}",
+        }),
+      ],
+    });
   }
 }
