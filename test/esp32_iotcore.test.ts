9858b247e415c6c9fd8058a7104b6e4c8fa8c048
raiha <68338762+raihalea@users.noreply.github.com>
2024-12-05 22:43:23 +0900

    Initial commit

---
+++ test/esp32_iotcore.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Esp32Iotcore from '../lib/esp32_iotcore-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/esp32_iotcore-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Esp32Iotcore.Esp32IotcoreStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
